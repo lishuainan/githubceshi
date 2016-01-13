74249e31270c04cc9312d35b001aa4a5990e4347
lishuainan <17710366205.li>
2016-01-13 10:57:55 +0800

    创建新工程

---
+++ gti/gti/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gti
+//
+//  Created by LSN on 16/1/13.
+//  Copyright (c) 2016年 LSN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gti/gti/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gti
+//
+//  Created by LSN on 16/1/13.
+//  Copyright (c) 2016年 LSN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
